579d39cccf89c7a5bf7730bbdef34d71c0abefad
Alexander Nemtsov <alexander.nemtsov@gmail.com>
2018-04-04 23:14:57 +0300

    fixing demorequest autoload

--- www/vampismo/demorequest.php
+++ www/vampismo/demorequest.php
@@ -7,14 +7,7 @@
  */
 
 //Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/SMTP.php';
-
-
-
-
+use PHPMailer\PHPMailer\PHPMailer
 
 require '../vendor/autoload.php';
 
