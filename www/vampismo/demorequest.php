c42e188cbd075960d51348f4e156ef91470d23d9
Alexander Nemtsov <alexander.nemtsov@gmail.com>
2018-04-05 18:39:50 +0300

    test for submit form

--- www/vampismo/demorequest.php
+++ www/vampismo/demorequest.php
@@ -1,97 +1,79 @@
 <?php
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- * This uses traditional id & password authentication - look at the gmail_xoauth.phps
- * example to see how to use XOAUTH2.
- * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
- */
 
 //Import PHPMailer classes into the global namespace
 use PHPMailer\PHPMailer\PHPMailer;
 
 require '../vendor/autoload.php';
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
+$msg = '';
 
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
+//Don't run this unless we're handling a form submission
+if (array_key_exists('email', $_POST)) {
+    date_default_timezone_set('Etc/UTC');
 
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
+    require '../vendor/autoload.php';
 
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
 
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
+    // sanitize the inputs
+    $client_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $client_country = filter_var($_POST['country'], FILTER_SANITIZE_STRING);
+    $client_zip = filter_var($_POST['zipcode'], FILTER_SANITIZE_STRING);
+    $client_address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
 
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
+    //Create a new PHPMailer instance
+    $mail = new PHPMailer;
+    //Tell PHPMailer to use SMTP
+    $mail->isSMTP();
 
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
 
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "alexander.nemtsov@gmail.com";
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    // use
+    // $mail->Host = gethostbyname('smtp.gmail.com');
+    // if your network does not support SMTP over IPv6
 
-//Password to use for SMTP authentication
-$mail->Password = "pdrvoqperqhwjdxt";
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
 
-//Set who the message is to be sent from
-$mail->setFrom('alexander.nemtsov@gmail.com', 'Alexander Nemtsov');
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "alexander.nemtsov@gmail.com";
+    //Password to use for SMTP authentication
+    $mail->Password = "pdrvoqperqhwjdxt";
 
-//Set an alternative reply-to address
-$mail->addReplyTo('alexander.nemtsov@gmail.com', 'Alexaner Nemtsov');
+    //Set who the message is to be sent from
+    $mail->setFrom('alexander.nemtsov@gmail.com', 'Alexander Nemtsov');
+    //Set an alternative reply-to address
+    $mail->addReplyTo('alexander.nemtsov@gmail.com', 'Alexaner Nemtsov');
+    //Set who the message is to be sent to
+    $mail->addAddress('alexander.nemtsov@gmail.com', 'Vam Pismo');
 
-//Set who the message is to be sent to
-$mail->addAddress('alexander.nemtsov@gmail.com', 'Vam Pismo');
+    //Set the subject line
+    $mail->Subject = 'New Task :: Demo Request';
 
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-
-$mail->Body = <<<EOT
-Email: privet
-Name: alo alo alo
-Message: zzza aa asasd
+    $mail->Body = <<<EOT
+Name:  {$client_name}
+Country: {$client_country}
+Address: {$client_zip} - {$client_address}
 EOT;
 
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.png');
+    //send the message, check for errors
+    if (!$mail->send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+        $msg = json_encode(array('success' => false))
+    } else {
+        echo "Message sent!";
+        $msg = json_encode(array('success' => true))
+    }
 
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
 }
 
-//Section 2: IMAP
-//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
-//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
-//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
-//be useful if you are trying to get this working on a non-Gmail IMAP server.
-function save_mail($mail)
-{
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
-
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-
-    return $result;
-}
+die($msg)
---
+++ www/vampismo/js.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  var form = $('#form'),
+      country = $('#country'),
+      name = $('#name'),
+      zipcode = $('#zipcode'),
+      address = $('#address'),
+      info = $('#info'),
+      submit = $("#submit");
+  
+  form.on('input', '#country, #name, #zipcode, #address', function() {
+    $(this).css('border-color', '');
+    info.html('').slideUp();
+  });
+  
+  submit.on('click', function(e) {
+    e.preventDefault();
+    if(validate()) {
+      $.ajax({
+        type: "POST",
+        url: "demorequest.php",
+        data: form.serialize(),
+        dataType: "json"
+      }).done(function(data) {
+        if(data.success) {
+          country.val('');
+          name.val('');
+          zipcode.val('');
+          address.val('');
+          info.html('Message sent!').css('color', 'green').slideDown();
+        } else {
+          info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+        }
+      });
+    }
+  });
+  
+  function validate() {
+    var valid = true;
+// email regex    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    
+    if($.trim(country.val()) {
+      country.css('border-color', 'red');
+      valid = false;
+    }
+    if($.trim(name.val()) === "") {
+      name.css('border-color', 'red');
+      valid = false;
+    }
+    if($.trim(zipcode.val()) === "") {
+      message.css('border-color', 'red');
+      valid = false;
+    }
+    if($.trim(address.val()) === "") {
+      message.css('border-color', 'red');
+      valid = false;
+    }
+    
+    return valid;
+  }
+
+});
