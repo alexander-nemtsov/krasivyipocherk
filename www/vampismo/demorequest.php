e1fa9e58ebcd4b9fdbb2651947804e5b41ba5105
Alexander Nemtsov <alexander.nemtsov@gmail.com>
2018-04-05 20:57:28 +0300

    trying to debug php

--- www/vampismo/demorequest.php
+++ www/vampismo/demorequest.php
@@ -7,6 +7,11 @@ require '../vendor/autoload.php';
 
 $msg = '';
 
+
+echo("New Demo Request")
+echo($_POST['address'])
+
+
 //Don't run this unless we're handling a form submission
 if (array_key_exists('address', $_POST)) {
     date_default_timezone_set('Etc/UTC');
