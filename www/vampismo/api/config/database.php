8d5834aa5bf6929e1f362f8782dc178d130a5a02
Alexander Nemtsov <alexander.nemtsov@gmail.com>
2018-04-14 08:02:33 +0300

    database settings

--- www/vampismo/api/config/database.php
+++ www/vampismo/api/config/database.php
@@ -3,6 +3,7 @@ class Database{
  
     // specify your own database credentials
     private $host = "172.18.0.3";
+    private $port = "3306";
     private $db_name = "api_db";
     private $username = "root";
     private $password = "secret";
